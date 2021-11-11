df0b5bb0a42ad2dc2bf0c849e928bf23f63e4e2f
devika <devikavarshney0003@gmail.com>
2021-11-12 00:06:38 +0530

    changes done

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 class Navbar {
-    constructor(target, menu) {
-        if (target instanceof HTMLElement && menu instanceof HTMLElement) {
-            this.btn = target;
-            this.menu = menu;
+  constructor(target, menu) {
+    if (target instanceof HTMLElement && menu instanceof HTMLElement) {
+      this.btn = target;
+      this.menu = menu;
 
-            this.btn.addEventListener('click', () => {
-                this.open();
-            });
-        } else {
-            throw new TypeError("The Target and Menu arguments must be a DOM element."); 
-        }
-        
+      this.btn.addEventListener("click", () => {
+        this.open();
+      });
+    } else {
+      throw new TypeError(
+        "The Target and Menu arguments must be a DOM element."
+      );
     }
-    open() {
-        if (this.isopen) {
-            this.menu.classList.add('nav-hidden');
-        } else {
-            this.menu.classList.remove('nav-hidden');
-        }
+  }
+  open() {
+    if (this.isopen) {
+      this.menu.classList.add("nav-hidden");
+    } else {
+      this.menu.classList.remove("nav-hidden");
+    }
 
-        this.isopen = !this.isopen;
-    }
+    this.isopen = !this.isopen;
+  }
+}
+document.getElementById("card2").style.display = "none";
+
+var x = document.getElementById("card1");
+var y = document.getElementById("card2");
+function two() {
+  x.style.display = "none";
+  y.style.display = "block";
+}
+function one() {
+  y.style.display = "none";
+  x.style.display = "block";
 }
