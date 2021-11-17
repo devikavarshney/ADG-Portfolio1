a77fed6a55579fc3081be38ff6f66b4061a1393b
Neha Valeti <72661758+nehavaleti@users.noreply.github.com>
2021-11-17 09:51:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,27 @@ class Navbar {
     this.isopen = !this.isopen;
   }
 }
+var box1 = document.querySelector(".box1");
+var box2 = document.querySelector(".box2");
+var box3 = document.querySelector(".box3");
+var para1 = document.querySelector(".para1");
+var para2 = document.querySelector(".para2");
+var ro1 = document.querySelector(".ro1");
+var ro2 = document.querySelector(".ro2");
+
+fetch('./file.json')
+ .then((response)=>{return response.json()})
+ .then((result)=>{result.map((item)=>{
+    box1.innerHTML+='<div class="box_info"><img src='+item.box1img+'  class="box1_img"><span class="box1_info">'+item.box1heading+'</span></div>'
+    box2.innerHTML+='<div class="box_info"><img src='+item.box2img+' class="box2_img"><span class="box2_info">'+item.box2heading+'</span></div>'
+    box3.innerHTML+='<div class="box_info"><img src='+item.box3img+' class="box3_img"><span class="box3_info">'+item.box3heading+'</span></div>'
+    para1.innerHTML+='<div class="ele"><h2>'+item.para1heading+'</h2></div><p class="para1_info">'+item.para1para+'</p>'
+    para2.innerHTML+='<div class="hea"><h2>'+item.para2heading+'</h2></div> <h5 class="p2">'+item.para2para+'</h5>'
+    ro1.innerHTML+='<div class="f1"><i class="lni lni-checkmark-circle"></i>  <span class="point">'+item.f1heading+'</span></div><div class="s1"><i class="lni lni-checkmark-circle"></i>  <span class="point">'+item.s1heading+'</span></div>'
+    ro2.innerHTML+='<div class="f2"><i class="lni lni-checkmark-circle"></i>  <span class="point">'+item.f2heading+'</span></div><div class=" s2"><i class="lni lni-checkmark-circle"></i>  <span class="point">'+item.s2heading+'</span></div>'
+    
+ })})
+
 document.getElementById("card2").style.display = "none";
 
 var x = document.getElementById("card1");
